aab610c4086f83370a663e5d9453b82073f40366
Manuel Sanchez <manuss20@gmail.com>
2021-10-05 22:35:31 +0200

    Update Samples

---
+++ API_Telemetry/Controllers/WindTurbineController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using API_Telemetry.Models;
+
+namespace API_Telemetry.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WindTurbineController : ControllerBase
+    {
+        private readonly ILogger<WindTurbineController> _logger;
+
+        public WindTurbineController(ILogger<WindTurbineController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WindTurbine> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 1).Select(index => new WindTurbine
+            {
+                Date = DateTime.Now.AddDays(index),
+                Temperature = rng.Next(-20, 55)
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ API_Telemetry/Models/WindTurbine.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace API_Telemetry.Models
+{
+    public class WindTurbine
+    {
+        public DateTime Date { get; set; }
+        public int Temperature { get; set; }
+    }
+}
